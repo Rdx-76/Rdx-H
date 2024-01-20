5fcd9012fe9464f7e9e29659e10c81fa8928e89e
Rdx <132105892+Rdx-76@users.noreply.github.com>
2024-01-20 22:14:19 +0600

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -3,7 +3,7 @@ os.system("git pull")
 import time
 import os
 
-def print_welcome(speed):
+def print_welcome(speed, welcome_size, rdx_tech_size, version_size):
     message = "Welcome Boss"
     
     for char in message:
@@ -11,20 +11,19 @@ def print_welcome(speed):
         time.sleep(1 / (speed * 0.1))  # Adjust speed here
     print("\033[0m")  # Reset color
 
-def print_footer():
-    brand_text = 'RDX TECH'
-    version_text = '[ Version 00.00.01 ]'
-    
-    print(f"\033[1;36m{brand_text}\033[0m \033[1;36m{version_text}\033[0m")
+    print(f"\033[1;33m{'RDX TECH':<{rdx_tech_size}}\033[0m", end='')
+    print(f"\033[1;33m[ Version 00.00.01 ]\033[0m")
 
 def main():
     speed = 50  # Adjust the speed from 1 to 100
+    welcome_size = 16
+    rdx_tech_size = 10
+    version_size = 6
     
     print("\033[40m")  # Set background color to black
     print(" " * os.get_terminal_size().columns)  # Adjust space for the black background
     
-    print_welcome(speed)
-    print_footer()
+    print_welcome(speed, welcome_size, rdx_tech_size, version_size)
 
     # Call another script after writing "Welcome Boss"
     os.system("python rdx.py")
