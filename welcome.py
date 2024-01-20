a65965280198cd682d1b4a311d3e84f4ba81f565
Rdx <132105892+Rdx-76@users.noreply.github.com>
2024-01-20 22:11:23 +0600

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -4,13 +4,18 @@ import time
 import os
 
 def print_welcome(speed):
-    os.system('clear')
     message = "Welcome Boss"
     
     for char in message:
         print(f"\033[1;36m{char}", end='', flush=True)
         time.sleep(1 / (speed * 0.1))  # Adjust speed here
     print("\033[0m")  # Reset color
+
+def print_footer():
+    brand_text = 'RDX TECH'
+    version_text = '[ Version 00.00.01 ]'
+    
+    print(f"\033[1;36m{brand_text}\033[0m \033[1;36m{version_text}\033[0m")
 
 def main():
     speed = 50  # Adjust the speed from 1 to 100
@@ -19,6 +24,7 @@ def main():
     print(" " * os.get_terminal_size().columns)  # Adjust space for the black background
     
     print_welcome(speed)
+    print_footer()
 
     # Call another script after writing "Welcome Boss"
     os.system("python rdx.py")
