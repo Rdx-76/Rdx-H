ba8b4eb04da30ca2d91c36398b656231a9e9a86e
Rdx <132105892+Rdx-76@users.noreply.github.com>
2024-01-20 22:18:03 +0600

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -3,6 +3,16 @@ os.system("git pull")
 import time
 import os
 
+def print_centered(text, size):
+    columns = os.get_terminal_size().columns
+    padding = (columns - size) // 2
+    print(f"\033[1;36m{' ' * padding}{text}\033[0m")
+
+def print_lower(text, size):
+    columns = os.get_terminal_size().columns
+    padding = (columns - size) // 2
+    print(f"\033[1;33m{' ' * padding}{text}\033[0m")
+
 def print_welcome(speed, welcome_size, rdx_tech_size, version_size):
     message = "Welcome Boss"
     
@@ -11,14 +21,14 @@ def print_welcome(speed, welcome_size, rdx_tech_size, version_size):
         time.sleep(1 / (speed * 0.1))  # Adjust speed here
     print("\033[0m")  # Reset color
 
-    print(f"\033[1;33m{'RDX TECH':<{rdx_tech_size}}\033[0m", end='')
-    print(f"\033[1;33m[ Version 00.00.01 ]\033[0m")
+    print_lower('RDX TECH', rdx_tech_size)
+    print_lower(f'[ Version 00.00.01 ]', version_size)
 
 def main():
     speed = 50  # Adjust the speed from 1 to 100
-    welcome_size = 106
-    rdx_tech_size = 80
-    version_size = 40
+    welcome_size = 16
+    rdx_tech_size = 10
+    version_size = 6
     
     print("\033[40m")  # Set background color to black
     print(" " * os.get_terminal_size().columns)  # Adjust space for the black background
